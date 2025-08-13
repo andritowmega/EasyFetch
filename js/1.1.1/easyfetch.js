1b6cb4e33aa717c0b312719d8714966a096824f6
Andres Carrasco Q. <38218181+andritowmega@users.noreply.github.com>
2025-08-13 09:55:33 -0500

    fix :bug: lifetime cookie

---
+++ js/1.1.1/easyfetch.js
@@ -0,0 +1,159 @@
+class EasyFetch {
+    constructor() {
+      this.production = false;
+      this.urlBase = "https://corefmvp2.smarttech.pe/fm/api/testproyect";
+      this.strings = {
+        aFieldToPayloadIsVoid: "Un campo está vacio",
+      };
+      this.inputs = this.getInputs();
+    }
+  
+    getInputs() {
+      return document.querySelectorAll("input,TextArea,select");
+    }
+  
+    getById(id) {
+      return document.getElementById(id);
+    }
+    
+    getByName(name) {
+      return document.getElementsByName(name);
+    }
+  
+    async fetchData(
+      urlApi,
+      params = {},
+      method = "GET",
+      verify = false,
+      headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      }
+    ) {
+      params = this.formatParams(params);
+
+      /*let url = this.production
+        ? "https://" + window.location.host + urlApi
+        : window.location.protocol+"//"+ window.location.host + urlApi;*/
+      let url = this.urlBase+urlApi;
+      if (!this.production) {
+        console.log("FETCHING", method, url);
+      }
+      if (verify) {
+        let checkedParams = this.checkParams(params);
+        if (!checkedParams.status) {
+          console.log("VERIFY", "Fetch Stoped");
+          //alert(this.strings.aFieldToPayloadIsVoid);
+          throw new Error(checkedParams.data);
+        }
+      }
+      const response = await fetch(url, {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        method: method,
+        body: JSON.stringify(params),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      if (!this.production && data) {
+        console.log("FETCH DATA", data);
+      }
+      return data;
+    }
+    formatParams(params) {
+      const traverse = (obj) => {
+        for (let key in obj) {
+          if (typeof obj[key] === 'object') {
+            traverse(obj[key]);
+          } else {
+            for (let y = 0; y < this.inputs.length; y++) {
+              if (obj[key] === this.inputs[y].id) {
+                obj[key] = this.inputs[y].value;
+                break; // Finaliza el bucle for cuando se encuentra una coincidencia
+              }
+            }
+          }
+        }
+      };
+    
+      traverse(params);
+    
+      if (!this.production) {
+        console.log("PARAMS PAYLOAD", params);
+      }
+    
+      return params;
+    }
+    checkParams(params) {
+      for (let i in params) {
+        if (!params[i] || params[i] == null || params[i] == undefined) {
+          console.log("VERIFY The value '" + i + "' is void");
+          return {
+            status:false,
+            data:"El campo "+i+" está vacio"
+          };
+        }
+      }
+      return {
+        status:true,
+        data:"Formulario completo"
+      };
+    }
+    setCookie(name,value,path="/",lifetime="1"){
+      const date = new Date();
+      date.setTime(date.getTime() + (lifetime*24*60*60*1000));
+      const expires = "expires=" + date.toUTCString();
+      document.cookie = `${name}=${value}; ${expires}; path=${path}`;
+    }
+    getCookie(cname) {
+      let name = cname + "=";
+      let decodedCookie = decodeURIComponent(document.cookie);
+      let ca = decodedCookie.split(';');
+      for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+    printJSONinHTML(jsonObj, parentElement) {
+            var outputHTML = '<ul>';
+            
+            for (var key in jsonObj) {
+                if (jsonObj.hasOwnProperty(key)) {
+                    outputHTML += '<li>';
+                    outputHTML += '<strong>' + key + ':</strong> ';
+                    
+                    if (typeof jsonObj[key] === 'object') {
+                        // Recursivamente imprimir sub-objetos
+                        imprimirJSONenHTML(jsonObj[key], outputHTML);
+                    } else {
+                        // Imprimir valores simples
+                        outputHTML += jsonObj[key];
+                    }
+                    
+                    outputHTML += '</li>';
+                }
+            }
+            
+            outputHTML += '</ul>';
+            
+            // Agregar HTML al elemento padre
+            parentElement.innerHTML = outputHTML;
+        }
+  }
+  let easyFetch = null;
+  document.addEventListener("DOMContentLoaded", function() {
+    easyFetch = new EasyFetch();
+
+  });
+  
+  
